a32ae39fa4540e87a5833b09398338ad50ff8d0c
ivetteramos <101986174+ivetteramos@users.noreply.github.com>
2022-06-29 14:25:02 -0700

    saving changes to pull from Jeremiah

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,26 @@
+var div = document.getElementById("textarea");
+var btn = document.getElementById("btn1");
 
+btn.addEventListener("click", function () {
+  fetch("https://quotes15.p.rapidapi.com/quotes/random/", options)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      var quote = document.createElement("textarea");
+      quote.textContent = data.content;
+      div.appendChild(quote);
+      var element = document.getElementById("btn1");
+      element.remove();
+    });
+});
+
+var options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "ee4ba4d88emsh5f022dd384ca11cp1c3043jsn048617c942bf",
+    "X-RapidAPI-Host": "quotes15.p.rapidapi.com",
+  },
+};
 
